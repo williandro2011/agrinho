e3f632fc45cbeaf124a3981f0bcd1d2e137101c9
williandro2011 <williandro.schmidt@escola.pr.gov.br>
2026-07-07 20:45:11 -0300

    1.0

--- script.js
+++ script.js
@@ -1,9 +1,73 @@
+// ===============================
+// LENIS
+// ===============================
+
+const lenis = new Lenis({
+    duration: 1.2,
+    smoothWheel: true
+});
+
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
+// ===============================
+// CURSOR GLOW
+// ===============================
+
 const glow = document.getElementById("cursorGlow");
 
-document.addEventListener("mousemove",(e)=>{
+window.addEventListener("mousemove", (e) => {
 
-    glow.style.left=e.clientX+"px";
+    glow.animate({
 
-    glow.style.top=e.clientY+"px";
+        left: e.clientX + "px",
+        top: e.clientY + "px"
+
+    }, {
+
+        duration: 300,
+        fill: "forwards"
+
+    });
+
+});
+
+// ===============================
+// GSAP
+// ===============================
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".hero-content",{
+
+    opacity:0,
+
+    y:120,
+
+    duration:1.4,
+
+    ease:"power4.out"
+
+});
+
+gsap.from(".paper",{
+
+    scrollTrigger:{
+
+        trigger:".paper",
+
+        start:"top 80%"
+
+    },
+
+    opacity:0,
+
+    y:120,
+
+    duration:1.3
 
 });
